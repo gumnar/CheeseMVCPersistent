375fc2c58bcc5697f46747958a8722e1c07542d2
Gunnar Smith <gunnarsmith314@gmail.com>
2018-06-21 13:22:45 -0500

    - Created CheeseCategory class to eventually replace the enum - CheeseCategories are now stored in the DB between sessions

---
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CheeseMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.Models;
+using CheeseMVC.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CheeseMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly CheeseDbContext context;
+
+        public CategoryController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            List<CheeseCategory> categories = context.Categories.ToList();
+            ViewBag.Title = "Categories";
+            return View(categories);
+        }
+
+        public IActionResult Add()
+        {
+            AddCategoryViewModel addCategoryViewModel = new AddCategoryViewModel();
+            return View(addCategoryViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddCategoryViewModel addCategoryViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(addCategoryViewModel);
+            }
+
+            CheeseCategory newCategory = new CheeseCategory
+            {
+                Name = addCategoryViewModel.Name
+            };
+
+            // Add newCategory to the DbContext instance and then save it to the DB
+            context.Categories.Add(newCategory);
+            context.SaveChanges();
+
+            // Redirect to the Index action of the Category controller
+            return Redirect("/Category");
+        }
+    }
+}
---
+++ src/CheeseMVC/ViewModels/AddCategoryViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CheeseMVC.ViewModels
+{
+    public class AddCategoryViewModel
+    {
+        [Display(Name = "Category Name")]
+        public string Name { get; set; }
+    }
+}
